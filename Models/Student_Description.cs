403cac3165d8b856bf7e2acaa1a7e2defcfd5015
Ashraful Haque <ashraful.haque@post.com>
2021-03-09 20:50:06 +0000

    updated student controller

--- Models/Student_Description.cs
+++ Models/Student_Description.cs
@@ -6,6 +6,7 @@ namespace Models
     {
         [Key]
         public int Id { get; set; }
+        public int Students_id { get; set; }
         public int age { get; set; }
         public string first_name { get; set; }
         public string last_name { get; set; }
